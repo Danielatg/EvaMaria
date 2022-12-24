78a96fa5b2715e0098bb21de2711a64e2b8ed621
Danielatg <92417884+Danielatg@users.noreply.github.com>
2022-12-24 10:52:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,11 +1,12 @@
 class script(object):
     START_TXT = """𝐘𝐨..𝐘𝐨.. {},
 <b>I'm Powerful Auto-Filter Bot You Can Use Me As A Auto-filter in Your Group ....</b>\n\n<b>Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There...</b>🤓\n\n⚠️<b>More Help Check Help Button Below</b>\n\n©️MᴀɪɴᴛᴀɪɴᴇD Bʏ  <a href=tg://user?id=633942759>ᴘʜʏsɪᴄ_ʜʏʙʀɪᴅ🇵🇹/🇦🇪</a> """
-    HELP_TXT = """b>○  it's Note Complicated...</b>🤓
+    HELP_TXT = """ <b>○  it's Note Complicated...</b>🤓
 <b>○  Search using inline mode
 This methord works on any chat, Just type @TGMovieRobot and then leave a space and search any movie you want...</b>
 
 <b>○ <u>Notice </u>📙:-</b>
+
 <b>○Dont Spam Me...</b>🤒\n\n©️MᴀɪɴᴛᴀɪɴᴇD Bʏ <a href=tg://user?id=633942759>ᴘʜʏsɪᴄ_ʜʏʙʀɪᴅ🇵🇹/🇦🇪</a>
 """
     ABOUT_TXT = """<b>○ My Name :</b> <code>Auto-Filter Bot</code>
